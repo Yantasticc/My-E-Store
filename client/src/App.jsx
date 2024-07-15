93436a2a96028a49f6a398580c13e293c00f8c47
Yantasticc <yashkalamkhede2002@gmail.com>
2024-07-15 20:50:03 +0530

    add to cart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./pages/authPage";
 import Items from "./pages/homePage";
 import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext';
 import ProtectedRoute from './components/ProtectedRoute';
+import Cart from "./pages/cartPage";
 
 const App = () => {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Auth />} />
-          <Route path="/home" element={<ProtectedRoute element={<Items />} />} />
-        </Routes>
-      </BrowserRouter>
+      <CartProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Auth />} />
+            <Route path="/home" element={<ProtectedRoute element={<Items />} />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
     </AuthProvider>
   )
 }
--- client/src/pages/homePage/index.jsx
+++ client/src/pages/homePage/index.jsx
@@ -1,3 +1,4 @@
+import { AiOutlineShoppingCart } from "react-icons/ai"; 
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import AddItem from '../../components/AddItem';
@@ -5,11 +6,13 @@ import AllItems from '../../components/AllItems';
 import DeleteItem from '../../components/DeleteItem';
 import UpdateItem from '../../components/UpdateItem';
 import { useAuth } from '../../context/AuthContext';
+import { useCart } from '../../context/CartContext';
 
 const Items = () => {
   const [activeComponent, setActiveComponent] = useState('all');
   const [searchTerm, setSearchTerm] = useState('');
   const { logout } = useAuth();
+  const { cartItems } = useCart();
   const navigate = useNavigate();
 
   const renderComponent = () => {
@@ -31,6 +34,10 @@ const Items = () => {
     logout();
     navigate('/');
     console.clear();
+  }
+
+  const handleClickCart = () => {
+    navigate('/cart');
   }
 
   return (
@@ -66,11 +73,17 @@ const Items = () => {
               🔍
             </button>
           </div>
+          <button className='bg-blue-500 text-white px-6 py-3 rounded-full text-lg hover:bg-blue-700 transition duration-300' onClick={handleClickCart}>
+            <div className="flex items-center">
+              <AiOutlineShoppingCart size={24} className="mr-2" /> {cartItems.length}
+            </div>
+          </button>
           <button 
             onClick={handleLogout}
             className='bg-red-500 text-white px-6 py-3 rounded-full text-lg hover:bg-red-700 transition duration-300'
           >
-            Logout</button>
+            Logout
+          </button>
         </div>
       </header>
       <div className="container mx-auto p-4">
